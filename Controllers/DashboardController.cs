c77430736da685fba6f3505fd890dae8f1955526
Ahmad Alhaj Asaad <79484313+AhmadAlhajAsaad@users.noreply.github.com>
2024-10-30 10:25:39 +0100

    Add files via upload
    Complete1

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Migration_Tool_GraphAPI.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Dashboard/TransferFile
+        [HttpPost]
+        public ActionResult TransferFile(HttpPostedFileBase file, string destinationLink)
+        {
+            if (file != null && !string.IsNullOrEmpty(destinationLink))
+            {
+                // Add logic here to handle file upload and transfer to the specified link
+                // This can include validation, file processing, and API calls for file transfer
+
+                ViewBag.Message = "File transfer initiated successfully!";
+            }
+            else
+            {
+                ViewBag.Message = "Please select a file and enter a destination link.";
+            }
+
+            return View("Index");
+        }
+    }
+}
+
+//using Microsoft.Graph;
+//using Migration_Tool_GraphAPI.Helpers;
+//using System;
+//using System.IO;
+//using System.Threading.Tasks;
+//using System.Web;
+//using System.Web.Mvc;
+//using System.Configuration;
+
+//namespace Migration_Tool_GraphAPI.Controllers
+//{
+//    public class DashboardController : Controller
+//    {
+//        public static string appId = ConfigurationManager.AppSettings["ida:AppId"];
+//        // Method to render the dashboard view
+//        public ActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        // Start transfer action
+//        [HttpPost]
+//        public async Task<ActionResult> StartTransfer(HttpPostedFileBase file, string destinationUrl)
+//        {
+//            if (file == null || string.IsNullOrWhiteSpace(destinationUrl))
+//            {
+//                ViewBag.Error = "File or destination URL is missing.";
+//                return View("Index");
+//            }
+
+//            try
+//            {
+//                // Get the Microsoft Graph client
+//                var graphClient = GraphHelper.GetAuthenticatedClient();
+
+//                // Read the file into a byte array
+//                byte[] fileData;
+//                using (var binaryReader = new BinaryReader(file.InputStream))
+//                {
+//                    fileData = binaryReader.ReadBytes(file.ContentLength);
+//                }
+
+//                // Extract SharePoint site and path info from the destination URL
+//                var siteId = appId; // Replace with actual Site ID
+//                var documentLibrary = "Documenten"; // Replace with the correct library name or path
+
+//                // Perform the upload
+//                var uploadResult = await graphClient
+//                    .Sites[siteId]
+//                    .Drives[documentLibrary]
+//                    .Root
+//                    .ItemWithPath(file.FileName)
+//                    .Content
+//                    .Request()
+//                    .PutAsync<DriveItem>(new MemoryStream(fileData));
+
+//                ViewBag.Message = "File uploaded successfully!";
+//            }
+//            catch (Exception ex)
+//            {
+//                ViewBag.Error = $"An error occurred: {ex.Message}";
+//            }
+
+//            return View("Index");
+//        }
+//    }
+//}
---
+++ Helpers/GraphHelper.cs
@@ -0,0 +1,132 @@
+﻿using Migration_Tool_GraphAPI.Models;
+using Microsoft.Graph;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+///Calender
+using Migration_Tool_GraphAPI.TokenStorage;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using Migration_Tool_GraphAPI;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Migration_Tool_GraphAPI.Helpers;
+using Owin;
+using Newtonsoft.Json;
+using System.Threading;
+using System.IO;
+using System;
+///
+namespace Migration_Tool_GraphAPI.Helpers
+{
+    public static class GraphHelper
+    {
+        // Load configuration settings from PrivateSettings.config
+        private static string appId = ConfigurationManager.AppSettings["ida:AppId"];
+        private static string appSecret = ConfigurationManager.AppSettings["ida:AppSecret"];
+        private static string redirectUri = ConfigurationManager.AppSettings["ida:RedirectUri"];
+        private static List<string> graphScopes =
+            new List<string>(ConfigurationManager.AppSettings["ida:AppScopes"].Split(' '));
+
+        public static async Task<IEnumerable<Event>> GetEventsAsync()
+        {
+            var graphClient = GetAuthenticatedClient();
+
+            var events = await graphClient.Me.Events.Request()
+                .Select("subject,organizer,start,end")
+                .OrderBy("createdDateTime DESC")
+                .GetAsync();
+
+            return events.CurrentPage;
+        }
+
+        private static GraphServiceClient GetAuthenticatedClient()
+        {
+            return new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        var idClient = ConfidentialClientApplicationBuilder.Create(appId)
+                            .WithRedirectUri(redirectUri)
+                            .WithClientSecret(appSecret)
+                            .Build();
+
+                        var tokenStore = new SessionTokenStore(idClient.UserTokenCache,
+                                HttpContext.Current, ClaimsPrincipal.Current);
+
+                        var userUniqueId = tokenStore.GetUsersUniqueId(ClaimsPrincipal.Current);
+                        var account = await idClient.GetAccountAsync(userUniqueId);
+
+                        // By calling this here, the token can be refreshed
+                        // if it's expired right before the Graph call is made
+                        var result = await idClient.AcquireTokenSilent(graphScopes, account)
+                            .ExecuteAsync();
+
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", result.AccessToken);
+                    }));
+        }
+
+        public static async Task<CachedUser> GetUserDetailsAsync(string accessToken)
+        {
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    async (requestMessage) =>
+                    {
+                        requestMessage.Headers.Authorization =
+                            new AuthenticationHeaderValue("Bearer", accessToken);
+                    }));
+
+            var user = await graphClient.Me.Request()
+                .Select(u => new {
+                    u.DisplayName,
+                    u.Mail,
+                    u.UserPrincipalName,
+
+                })
+                .GetAsync();
+
+            return new CachedUser
+            {
+                Avatar = string.Empty,
+                DisplayName = user.DisplayName,
+                Email = string.IsNullOrEmpty(user.Mail) ?
+                    user.UserPrincipalName : user.Mail
+            };
+        }
+        
+        //public static async Task<string> GetUserPhotoAsync(string accessToken)
+        //{
+        //    var graphClient = new GraphServiceClient(
+        //        new DelegateAuthenticationProvider(
+        //            requestMessage =>
+        //            {
+        //                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        //                return Task.CompletedTask;
+        //            }));
+
+        //    try
+        //    {
+        //        var photoStream = await graphClient.Me.Photo.Content.Request().GetAsync();
+        //        using (var memoryStream = new MemoryStream())
+        //        {
+        //            await photoStream.CopyToAsync(memoryStream);
+        //            var photoBytes = memoryStream.ToArray();
+        //            return $"data:image/jpeg;base64,{Convert.ToBase64String(photoBytes)}";
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        // Return a default icon if there's no profile photo
+        //        return null;
+        //    }
+        //}
+
+    }
+}
